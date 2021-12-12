1d7f8062266c445e62ea423c69e4937cb4a012d8
Daniel Martins <danidiamoo1@gmail.com>
2021-12-12 08:08:26 -0300

    final version 1.0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let form = document.querySelector('.validator');
 let validator = {
     handleSubmit: (event) =>{
         event.preventDefault();
-        let send = false;
+        let send = true;
 
         let inputs = form.querySelectorAll('input');
         let elementError = document.querySelector('.errorMessage');
@@ -12,6 +12,7 @@ let validator = {
         inputs.forEach((el)=>{
             let check = validator.checkInput(el);
             el.style.borderColor = '#999';
+            
             if(check !== true){
                 
                 send = false;
@@ -40,7 +41,18 @@ let validator = {
                     break;
 
                     case 'min':
+                            if(input.value.length < rDetails[1]){
+                                return `Campo deve ter pelo menos ${rDetails[1]} caracteres`;
+                            }
+                    break;
 
+                    case 'email':
+                            if(input.value != ''){
+                                let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                                if(!regex.test(input.value.toLowerCase())){
+                                    return 'E-mail inválido';
+                                }
+                            }
                     break;
                 }
             }
@@ -58,4 +70,6 @@ let validator = {
         elementError.style.display = 'flex';
     }
 }
+
+
 form.addEventListener('submit', validator.handleSubmit)
